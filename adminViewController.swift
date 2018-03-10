928e3a73cdeae61bf9fc2e4b77f09c77614e1681
nickpotts3 <32562264+nickpotts3@users.noreply.github.com>
2018-03-09 19:26:27 -0500

    added admin functionality

---
+++ adminViewController.swift
@@ -0,0 +1,25 @@
+//
+//  adminViewController.swift
+//  qrInvite
+//
+//  Created by Nick potts on 3/9/18.
+//  Copyright © 2018 Nexus. All rights reserved.
+//
+
+import UIKit
+
+class adminViewController: UIViewController {
+
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
--- signUpViewController.swift
+++ signUpViewController.swift
@@ -13,28 +13,44 @@ class signUpViewController: UIViewController {
 
     @IBOutlet weak var phoneNumTextField: UITextField!
     
+    @IBAction func admin_TouchUpInside(_ sender: Any) {
+        
+    }
+    @IBOutlet weak var adminButton: UIButton!
     @IBOutlet weak var signInButton: UIButton!
     @IBAction func signUpButton_TouchUpInside(_ sender: Any) {
         sendVerificationCode()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        adminButton.isHidden = true;
 
         
         // Do any additional setup after loading the view.
     }
 
     func sendVerificationCode() {
-        let phoneNumber = "+1" + (phoneNumTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines))!
-        UserDefaults.standard.set(phoneNumber, forKey: "phoneNumber")
         
-        PhoneAuthProvider.provider().verifyPhoneNumber(phoneNumber, uiDelegate: nil) { (verificationID, error ) in
-            if let error = error {
-                print("Phone number error" + error.localizedDescription)
-            }
-            else {
-            UserDefaults.standard.set(verificationID, forKey: "authVerificationID")
-            self.performSegue(withIdentifier: "signedIn", sender: nil)
+        if (phoneNumTextField.text == "kentAdmin") {
+            adminButton.isHidden = false
+        }
+        else {
+            let phoneNumber = "+1" + (phoneNumTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines))!
+            UserDefaults.standard.set(phoneNumber, forKey: "phoneNumber")
+        
+            PhoneAuthProvider.provider().verifyPhoneNumber(phoneNumber, uiDelegate: nil) { (verificationID, error ) in
+                if let error = error {
+                    print("Phone number error" + error.localizedDescription)
+                }
+                else {
+                    let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                    let vc: UIViewController = storyBoard.instantiateViewController(withIdentifier: "verifyCode")
+                    self.present(vc, animated: true, completion: {
+                        
+                    })
+                    UserDefaults.standard.set(verificationID, forKey: "authVerificationID")
+                   // self.performSegue(withIdentifier: "signedIn", sender: nil)
+                }
             }
         }
     }
