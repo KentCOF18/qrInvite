a732615f9fb20f0e9554fca6ab17a3a23df2f37d
KentCOF18 <37001476+KentCOF18@users.noreply.github.com>
2018-03-14 16:22:05 -0400

    Add files via upload

--- profileViewController.swift
+++ profileViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class profileViewController: UIViewController {
 
@@ -15,9 +17,41 @@ class profileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        getQRImage()
+        
         // Do any additional setup after loading the view.
     }
-
+    var databaseHandle: DatabaseHandle?
+   
+    func getQRImage() {
+        let uid = Auth.auth().currentUser?.uid
+        let ref = Database.database().reference()
+        databaseHandle = ref.child("users").child(uid!).child("url").observe(.childAdded)
+        { (snapshot) in
+            let qrCode = snapshot.value as? String
+            
+            if let actualCode = qrCode {
+                let imageURL = URL(string: actualCode)
+                var image: UIImage?
+                if let url = imageURL {
+                    DispatchQueue.global(qos: .userInitiated).async {
+                        let imageData = NSData(contentsOf: url)
+                        //All UI operations has to run on main thread.
+                        DispatchQueue.main.async {
+                            if imageData != nil {
+                                image = UIImage(data: imageData! as Data)!
+                                UserDefaults.standard.set(imageData, forKey: "profileQRCode")
+                                self.qrImage.image = image
+                                self.qrImage.sizeToFit()
+                            }
+                            
+                        }
+                    }
+                }
+            }
+            
+        }
+    }
 
 
 }
