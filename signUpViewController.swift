552ddeeda94ab8c09e21da2309cd7ab892fe65ed
nickpotts3 <32562264+nickpotts3@users.noreply.github.com>
2018-03-26 10:38:08 -0400

    Added animations to the two buttons on the signup page

--- signUpViewController.swift
+++ signUpViewController.swift
@@ -23,10 +23,23 @@ class signUpViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.hideKeyboardWhenTappedAround()
-        adminButton.isHidden = true
-        signInButton.backgroundColor = UIColor.lightGray
         
+        signInButton.center.x = self.view.frame.width + 300
+        
+        UIView.animate(withDuration: 1.5, delay: 0.5, usingSpringWithDamping: 1.0, initialSpringVelocity: 5.0, options: .curveEaseOut, animations: ({
+            
+            self.signInButton.center.x = self.view.frame.width / 2
+            
+        }), completion: nil)
+        
+        self.hideKeyboardWhenTappedAround()
+        
+        signInButton.backgroundColor = UIColor.lightGray
+        signInButton.layer.cornerRadius = 8.0
+        
+        adminButton.backgroundColor = UIColor.lightGray
+        adminButton.layer.cornerRadius = 8.0
+        adminButton.isHidden = true
         // Do any additional setup after loading the view.
     }
 
@@ -35,6 +48,7 @@ class signUpViewController: UIViewController {
         
         if (phoneNumTextField.text == "kentAdmin") {
             adminButton.isHidden = false
+            adminButton.pulsate()
         }
         else {
             let phoneNumber = "+1" + (phoneNumTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines))!
@@ -69,6 +83,22 @@ extension UIViewController {
     
     @objc func dismissKeyboard() {
         view.endEditing(true)
+    }
+}
+
+extension UIButton {
+    
+    func pulsate() {
+        let pulse = CASpringAnimation(keyPath: "transform.scale")
+        pulse.duration = 4.0
+        pulse.fromValue = 0.95
+        pulse.toValue = 1.0
+        pulse.autoreverses = true
+        pulse.repeatCount = 1
+        pulse.initialVelocity = 0.5
+        pulse.damping = 1.0
+        
+        layer.add(pulse, forKey: nil)
     }
 }
 
